50fbb4dcec37a2b4b9bb3dd78bd4127b2602a21e
ssageghi <ssagegghi@gmail.com>
2022-01-29 16:47:52 +0330

    Initial Commit

---
+++ helper/getDevelopers.js
@@ -0,0 +1,49 @@
+// https://github.com/trending/developers/javascript?since=monthly
+const cheerio = require("cheerio");
+const fetch = (...args) =>
+  import("node-fetch").then(({ default: fetch }) => fetch(...args));
+
+module.exports = function getDevelopers(FETCH_URL) {
+  return new Promise((resolve, reject) => {
+    fetch(FETCH_URL)
+      .then((res) => res.text())
+      .then((body) => {
+        const $ = cheerio.load(body);
+        const box = $(".Box article.Box-row");
+
+        let result = box.get().map((el) => {
+          const author = $(el).find(".h3>a").text().trim();
+          const username = $(el).find(".f4>a").text().trim();
+          const avatar = $(el).find(".mx-3 img").attr("src");
+
+          const reponame = $(el)
+            .find("article .h4.lh-condensed>a")
+            .text()
+            .trim();
+          const repoUrl = $(el)
+            .find("article .h4.lh-condensed>a")
+            .attr("href")
+            .trim();
+          const description = $(el)
+            .find("article .f6.text-gray.mt-1")
+            .text()
+            .trim();
+
+          return {
+            author,
+            username,
+            avatar,
+            url: "https://github.com/" + username,
+            reponame,
+            repourl: "https://github.com" + repoUrl,
+            description,
+          };
+        });
+
+        resolve(result);
+      })
+      .catch((e) => {
+        reject(e);
+      });
+  });
+};
---
+++ helper/main.js
@@ -0,0 +1,29 @@
+const getRepos = require("./getRepos");
+const getDevelopers = require("./getDevelopers");
+
+function getUrl(section, language, since, spoken_language_code) {
+  const BASE_URL = "https://github.com/trending";
+
+  let FETCH_URL = section ? BASE_URL + "/developers" : BASE_URL;
+
+  FETCH_URL =
+    (language ? FETCH_URL + "/" + language : FETCH_URL) + "?since=" + since;
+
+  FETCH_URL = spoken_language_code
+    ? FETCH_URL + "&spoken_language_code=" + spoken_language_code
+    : FETCH_URL;
+
+  return FETCH_URL;
+}
+
+function githubTrends({
+  section,
+  language,
+  since = "daily",
+  spoken_language_code,
+} = {}) {
+  const FETCH_URL = getUrl(section, language, since, spoken_language_code);
+
+  return section ? getDevelopers(FETCH_URL) : getRepos(FETCH_URL);
+}
+module.exports = githubTrends;
